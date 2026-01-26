9417404618b3d76a0e0966510099e60c081c8ec5
JBaurel <juniorbaurel@gmail.com>
2026-01-26 04:24:20 +0100

    Main modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,9 @@ import view.HauptFenster;
 
 public class Main {
     public static void main(String[] args) {
-        RennLogger.info("===========================================");
+        RennLogger.info("============================================");
         RennLogger.info("   F1 Rennsimulation - Nuerburgring GP");
-        RennLogger.info("===========================================");
+        RennLogger.info("============================================");
         RennLogger.info("Starte Anwendung...");
 
         // JavaFX HauptFenster starten
